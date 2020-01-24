1af46ece0fba67b70003e6b0c3d9890f1d408b42
htaebi <taeby.hamid@gmail.com>
2020-01-25 01:12:52 +0330

    'commit'

--- core/services/api-endpoints.service.ts
+++ core/services/api-endpoints.service.ts
@@ -1,21 +1,15 @@
 // Angular Modules
 import { Injectable } from '@angular/core';
 // Application Classes
-import { UrlBuilder } from '../../shared/classes/url-builder';
 import { QueryStringParameters } from '../../shared/classes/query-string-parameters';
-// Application Constants
-import { Constants } from 'src/app/config/constants';
-
+import { ApiUrlCreator } from './api-urlcreator';
+import { Constants } from '../../config/constants';
 @Injectable()
 // Returns the api endpoints urls to use in services in a consistent way
-export class ApiEndpointsService {
-
-    constructor(
-        // Application Constants
-        private constants: Constants
-    ) { }
-
-    /* #region EXAMPLES */
+export class ApiEndpointsService extends ApiUrlCreator {
+    constructor(constants: Constants) {
+        super(constants);
+     }    
     public getDataByIdEndpoint = (id: string): string => this.createUrlWithPathVariables('data', [id]);
 
     public getDataByIdAndCodeEndpoint = (id: string, code: number): string => this.createUrlWithPathVariables('data', [id, code]);
@@ -40,39 +34,4 @@ export class ApiEndpointsService {
     public getNewsEndpoint = (): string => this.createUrl('41gRGwOaw', true);
 
     public invalidUrlEndpoint = (): string => this.createUrl('invalidurl', true);
-    /* #endregion */
-
-    /* #region URL CREATOR */
-    // URL
-    private createUrl(action: string, isMockAPI: boolean = false): string {
-        const urlBuilder: UrlBuilder = new UrlBuilder(
-            isMockAPI ? this.constants.API_MOCK_ENDPOINT : this.constants.API_ENDPOINT,
-            action
-        );
-        return urlBuilder.toString();
-    }
-
-    // URL WITH QUERY PARAMS
-    private createUrlWithQueryParameters(action: string, queryStringHandler?: (queryStringParameters: QueryStringParameters) => void): string {
-        const urlBuilder: UrlBuilder = new UrlBuilder(this.constants.API_ENDPOINT, action);
-        // Push extra query string params
-        if (queryStringHandler) {
-            queryStringHandler(urlBuilder.queryString);
-        }
-        return urlBuilder.toString();
-    }
-
-    // URL WITH PATH VARIABLES
-    private createUrlWithPathVariables(action: string, pathVariables: any[] = []): string {
-        let encodedPathVariablesUrl: string = '';
-        // Push extra path variables
-        for (const pathVariable of pathVariables) {
-            if (pathVariable !== null) {
-                encodedPathVariablesUrl += `/${encodeURIComponent(pathVariable.toString())}`;
-            }
-        }
-        const urlBuilder: UrlBuilder = new UrlBuilder(this.constants.API_ENDPOINT, `${action}${encodedPathVariablesUrl}`);
-        return urlBuilder.toString();
-    }
-    /* #endregion */
 }
